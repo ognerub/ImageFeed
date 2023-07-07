6c076cbed8fdfb42f81506403a6f848bac183b0d
ognerub <a.ulianovskiy@yandex.ru>
2023-07-07 23:10:17 +0300

    first image loaded

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -7,31 +7,44 @@
 
 import UIKit
 
-class ImagesListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ImagesListViewController: UIViewController {
     
-
+    
     @IBOutlet private var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // connection of tableViw with it`s delegate and dataSource (now it`s MVC two in one) in code:
-        tableView.delegate = self
-        tableView.dataSource = self
-        // custom cell registration:
-        tableView.register(ImagesListCell.self, forCellReuseIdentifier: ImagesListCell.reuseIdentifier)
+//        // connection of tableViw with it`s delegate and dataSource (now it`s MVC two in one) in code:
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        // custom cell registration:
+//        tableView.register(ImagesListCell.self, forCellReuseIdentifier: ImagesListCell.reuseIdentifier)
     }
+}
 
+extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+
+        configCell(for: imageListCell, with: indexPath)
+
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) { }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
 }
 
