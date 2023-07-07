1e5cebbf023e61266102d480ba02b75bbb0565b1
ognerub <a.ulianovskiy@yandex.ru>
2023-07-07 21:38:09 +0300

    tableView created and connected with MVC

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ImagesListViewController: UIViewController {
 
+    @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
