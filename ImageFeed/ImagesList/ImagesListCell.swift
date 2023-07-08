b8b5898900d3119e45c08ceddd85fdbff6861e7d
ognerub <a.ulianovskiy@yandex.ru>
2023-07-08 13:37:08 +0300

    made some changes with custom cell and table to show all images, but have some problems with constraints

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     
-    static let reuseIdentifier = "ImagesListCell"        
+    static let reuseIdentifier = "ImagesListCell" 
+    
+    @IBOutlet weak var cellDateLabel: UILabel!
+    @IBOutlet weak var cellLikeButton: UIButton!
+    @IBOutlet weak var cellImage: UIImageView!
+           
     
 }
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ImagesListViewController: UIViewController {
     
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    } ()
     
     @IBOutlet private var tableView: UITableView!
     
@@ -21,14 +29,17 @@ class ImagesListViewController: UIViewController {
         //        // custom cell registration:
         //        tableView.register(ImagesListCell.self, forCellReuseIdentifier: ImagesListCell.reuseIdentifier)
         
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        
     }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
+    /// данный метод устанавливает количество строк (в нашем случае кастомных ячеек) в секции
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
-
+    /// данный метод опредетяет какую ячейку выводить, если нет кастомной, то отработает quard else и отобразится стандартная ячейка. Также запускается метод из extension для MVC - configCell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
 
@@ -43,10 +54,36 @@ extension ImagesListViewController: UITableViewDataSource {
 }
 
 extension ImagesListViewController {
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) { }
+    /// данный метод конфигурирует стиль кастомных ячеек, в частности присваивается картинка, если такая имеется (если нет, guard else вернет nil), форматируется дата, задается стиль кнопки лайка для четных и нечетных ячеек по indexPath.row)
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+
+        cell.cellImage.image = image
+        cell.cellDateLabel.text = dateFormatter.string(from: Date())
+
+        let isLiked = indexPath.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
+        cell.cellLikeButton.setImage(likeImage, for: .normal)
+    }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
+    /// в данном методе будем настраивать реагирование на нажатие пользователем на ячейку (строку)
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    
+    /// добавлен новый метод, корректирующий высоту ячейки (строки) в зависимости от высоты изображения
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
 }
 
