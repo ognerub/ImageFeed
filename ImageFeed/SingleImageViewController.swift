7bb9bf6060993ab9f20170c2da2113d952f70cd6
ognerub <a.ulianovskiy@yandex.ru>
2023-07-18 23:01:25 +0300

    single image view controller added

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Admin on 18.07.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
