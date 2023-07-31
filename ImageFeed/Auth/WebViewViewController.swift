bfd9c737e669a567f7a27c59846e6488e681d43b
ognerub <a.ulianovskiy@yandex.ru>
2023-07-31 22:58:07 +0300

    adding WebView outlet

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Admin on 31.07.2023.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    @IBOutlet private var WebView: WKWebView!
+}
