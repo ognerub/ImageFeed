0c0c46b8f4de72921b83380200d08db1df2d5764
ognerub <a.ulianovskiy@yandex.ru>
2023-09-14 11:59:35 +0300

    properties moved from ProfileVC to ProfilePresenter

--- ImageFeed/Profile/ProfilePresenter.swift
+++ ImageFeed/Profile/ProfilePresenter.swift
@@ -6,11 +6,102 @@
 //
 
 import Foundation
+import UIKit
 
-protocol ProfilePresenterProtocol {
-    var view: ProfileViewControllerProtocol? { get set }
+public protocol ProfilePresenterProtocol {
+    var viewController: ProfileViewControllerProtocol? { get set }
+    
 }
 
-final class ProfilePresenter {
-    var view: ProfileViewControllerProtocol?
+final class ProfilePresenter: UIView, ProfilePresenterProtocol {
+    
+    weak var viewController: ProfileViewControllerProtocol?
+    
+    var personImageView: UIImageView = {
+        let personImage = UIImage(named: "Avatar") ?? UIImage(systemName: "person.crop.circle.fill")!
+        let personImageView = UIImageView(image: personImage)
+        personImageView.translatesAutoresizingMaskIntoConstraints = false
+        return personImageView
+    }()
+    var personHashTagLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@ekaterina_now"
+        label.textColor = UIColor(named: "YP Grey")
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    var personNameLabel: UILabel = {
+        let personNameLabel = UILabel()
+        personNameLabel.text = "Екатерина Новикова"
+        personNameLabel.textColor = UIColor(named: "YP White")
+        personNameLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        personNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        return personNameLabel
+    }()
+    var personInfoTextLabel: UILabel = {
+        let personInfoTextLabel = UILabel()
+        personInfoTextLabel.text = "Hello, world!"
+        personInfoTextLabel.textColor = UIColor(named: "YP White")
+        personInfoTextLabel.font = UIFont.systemFont(ofSize: 13)
+        personInfoTextLabel.translatesAutoresizingMaskIntoConstraints = false
+        return personInfoTextLabel
+    }()
+    lazy var exitButton: UIButton = {
+        let exitButton = UIButton.systemButton(
+            with: UIImage(named: "LogOut") ?? UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: nil
+        )
+        exitButton.tintColor = UIColor(named: "YP Red")
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        return exitButton
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = UIColor(named: "YP Black")
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        addSubViews()
+        configureConstraints()
+    }
+}
+
+// MARK: - Setup views
+private extension ProfilePresenter {
+    func addSubViews() {
+        addSubview(personImageView)
+        addSubview(personNameLabel)
+        addSubview(personHashTagLabel)
+        addSubview(personInfoTextLabel)
+        addSubview(exitButton)
+    }
+    func configureConstraints() {
+        NSLayoutConstraint.activate([
+            personImageView.widthAnchor.constraint(equalToConstant: 70),
+            personImageView.heightAnchor.constraint(equalToConstant: 70),
+            personImageView.topAnchor.constraint(equalTo: topAnchor, constant: 76),
+            personImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            
+            personNameLabel.topAnchor.constraint(equalTo: personImageView.bottomAnchor, constant: 8),
+            personNameLabel.leadingAnchor.constraint(equalTo: personImageView.leadingAnchor),
+            
+            
+            personHashTagLabel.topAnchor.constraint(equalTo: personNameLabel.bottomAnchor, constant: 8),            personHashTagLabel.leadingAnchor.constraint(equalTo: personNameLabel.leadingAnchor),
+            
+            personInfoTextLabel.topAnchor.constraint(equalTo: personHashTagLabel.bottomAnchor, constant: 8),
+            personInfoTextLabel.leadingAnchor.constraint(equalTo: personHashTagLabel.leadingAnchor),
+            
+            exitButton.centerYAnchor.constraint(equalTo: personImageView.centerYAnchor),
+            exitButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+    }
 }
--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -8,13 +8,18 @@
 import UIKit
 import Kingfisher
 
-protocol ProfileViewControllerProtocol {
-    var presenter: ProfilePresenterProtocol? { get set }
+public protocol ProfileViewControllerProtocol: AnyObject {
+    var presenter: ProfilePresenterProtocol! { get set }
 }
 
 final class ProfileViewController: UIViewController, ProfileViewControllerProtocol {
     
-    var presenter: ProfilePresenterProtocol?
+    var presenter: ProfilePresenterProtocol!
+    
+    func configure(_ presenter: ProfilePresenterProtocol) {
+        self.presenter = presenter
+        self.presenter.viewController = self
+    }
     
     private let profileService = ProfileService.shared
     private let profileImageService = ProfileImageService.shared
@@ -25,54 +30,14 @@ final class ProfileViewController: UIViewController, ProfileViewControllerProtoc
     private var alertPresenter: AlertPresenterProtocol?
     private var profileImageServiceObserver: NSObjectProtocol?
     
-    var personImageView: UIImageView = {
-        let personImage = UIImage(named: "Avatar") ?? UIImage(systemName: "person.crop.circle.fill")!
-        let personImageView = UIImageView(image: personImage)
-        personImageView.translatesAutoresizingMaskIntoConstraints = false
-        return personImageView
-    }()
-    private let personHashTagLabel: UILabel = {
-        let label = UILabel()
-        label.text = "@ekaterina_now"
-        label.textColor = UIColor(named: "YP Grey")
-        label.font = UIFont.systemFont(ofSize: 13)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    private let personNameLabel: UILabel = {
-        let personNameLabel = UILabel()
-        personNameLabel.text = "Екатерина Новикова"
-        personNameLabel.textColor = UIColor(named: "YP White")
-        personNameLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        personNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        return personNameLabel
-    }()
-    private let personInfoTextLabel: UILabel = {
-        let personInfoTextLabel = UILabel()
-        personInfoTextLabel.text = "Hello, world!"
-        personInfoTextLabel.textColor = UIColor(named: "YP White")
-        personInfoTextLabel.font = UIFont.systemFont(ofSize: 13)
-        personInfoTextLabel.translatesAutoresizingMaskIntoConstraints = false
-        return personInfoTextLabel
-    }()
-    private lazy var exitButton: UIButton = {
-        let exitButton = UIButton.systemButton(
-            with: UIImage(named: "LogOut") ?? UIImage(systemName: "ipad.and.arrow.forward")!,
-            target: self,
-            action: #selector(Self.didTapButton)
-        )
-        exitButton.tintColor = UIColor(named: "YP Red")
-        exitButton.translatesAutoresizingMaskIntoConstraints = false
-        return exitButton
-    }()
+    override func loadView() {
+        view = ProfilePresenter()
+        setupExitButton()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         alertPresenter = AlertPresenterImpl(viewController: self)
-        view.backgroundColor = UIColor(named: "YP Black")
-        
-        addSubViews()
-        configureConstraints()
         
         guard let profile = profileService.profile else { return }
         updateProfileDetails(profile: profile)
@@ -102,7 +67,6 @@ final class ProfileViewController: UIViewController, ProfileViewControllerProtoc
         }
     }
     
-    
     @objc
     private func updateAvatar(notification: Notification) {
         guard
@@ -115,57 +79,35 @@ final class ProfileViewController: UIViewController, ProfileViewControllerProtoc
     }
     
     private func updateAvatar(url: URL) {
-        personImageView.kf.indicatorType = .activity
+        (view as? ProfilePresenter)?.personImageView.kf.indicatorType = .activity
         let processor = RoundCornerImageProcessor(cornerRadius: 61)
-        personImageView.kf.setImage(with: url, options: [.processor(processor)])
-        personImageView.layer.masksToBounds = true
-        personImageView.layer.cornerRadius = 34
+        (view as? ProfilePresenter)?.personImageView.kf.setImage(with: url, options: [.processor(processor)])
+        (view as? ProfilePresenter)?.personImageView.layer.masksToBounds = true
+        (view as? ProfilePresenter)?.personImageView.layer.cornerRadius = 34
     }
-    
+
     private func updateProfileDetails(profile: Profile) {
-        personNameLabel.text = profile.name
-        personHashTagLabel.text = profile.loginName
-        personInfoTextLabel.text = profile.bio
+        (view as? ProfilePresenter)?.personNameLabel.text = profile.name
+        (view as? ProfilePresenter)?.personHashTagLabel.text = profile.loginName
+        (view as? ProfilePresenter)?.personInfoTextLabel.text = profile.bio
     }
-    
-    private func addSubViews() {
-        view.addSubview(personImageView)
-        view.addSubview(personNameLabel)
-        view.addSubview(personHashTagLabel)
-        view.addSubview(personInfoTextLabel)
-        view.addSubview(exitButton)
-    }
-    
-    private func configureConstraints() {
-        NSLayoutConstraint.activate([
-            personImageView.widthAnchor.constraint(equalToConstant: 70),
-            personImageView.heightAnchor.constraint(equalToConstant: 70),
-            personImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 76),
-            personImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            
-            personNameLabel.topAnchor.constraint(equalTo: personImageView.bottomAnchor, constant: 8),
-            personNameLabel.leadingAnchor.constraint(equalTo: personImageView.leadingAnchor),
-            
-            personHashTagLabel.topAnchor.constraint(equalTo: personNameLabel.bottomAnchor, constant: 8),            personHashTagLabel.leadingAnchor.constraint(equalTo: personNameLabel.leadingAnchor),
-            
-            personInfoTextLabel.topAnchor.constraint(equalTo: personHashTagLabel.bottomAnchor, constant: 8),
-            personInfoTextLabel.leadingAnchor.constraint(equalTo: personHashTagLabel.leadingAnchor),
-            
-            exitButton.centerYAnchor.constraint(equalTo: personImageView.centerYAnchor),
-            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
-        ])
-    }
+
     
     private func switchToSplashViewController() {
         guard let window = UIApplication.shared.windows.first else { fatalError("Invalid Configuration of switchToSplashViewController") }
         window.rootViewController = SplashViewController()
     }
-    
+}
+
+// MARK: - Methods to show alert when exit button pressed
+extension ProfileViewController {
+    func setupExitButton() {
+        (view as? ProfilePresenter)?.exitButton.addTarget(self, action: #selector(didTapExitButton), for: .primaryActionTriggered)
+    }
     @objc
-    private func didTapButton() {
+    func didTapExitButton() {
         showAlertBeforExit()
     }
-    
     func showAlertBeforExit() {
         DispatchQueue.main.async {
             let model = AlertModel(
